b622e7d3727ac6f4e92945263793596098bfffa4
Ersin İşgör <ersinisgor@hotmail.com>
2024-03-31 11:08:34 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GeograpicInformationSystemProject
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.map = new GMap.NET.WindowsForms.GMapControl();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// map
+			// 
+			this.map.Bearing = 0F;
+			this.map.CanDragMap = true;
+			this.map.EmptyTileColor = System.Drawing.Color.Navy;
+			this.map.GrayScaleMode = false;
+			this.map.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+			this.map.LevelsKeepInMemory = 5;
+			this.map.Location = new System.Drawing.Point(12, 12);
+			this.map.MarkersEnabled = true;
+			this.map.MaxZoom = 2;
+			this.map.MinZoom = 2;
+			this.map.MouseWheelZoomEnabled = true;
+			this.map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+			this.map.Name = "map";
+			this.map.NegativeMode = false;
+			this.map.PolygonsEnabled = true;
+			this.map.RetryLoadTile = 0;
+			this.map.RoutesEnabled = true;
+			this.map.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+			this.map.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+			this.map.ShowTileGridLines = false;
+			this.map.Size = new System.Drawing.Size(699, 623);
+			this.map.TabIndex = 0;
+			this.map.Zoom = 0D;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(789, 80);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1058, 684);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.map);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private GMap.NET.WindowsForms.GMapControl map;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeograpicInformationSystemProject
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeMap();
+		}
+
+		private void InitializeMap()
+		{
+			map.DragButton = MouseButtons.Left;
+			map.MapProvider = GMapProviders.GoogleMap;
+			map.Position = new GMap.NET.PointLatLng(36.3275, 32.8187);
+			map.MinZoom = 4;
+			map.MaxZoom = 25;
+			map.Zoom = 10;
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			map.Dispose();
+			Application.Exit();
+		}
+	}
+}
